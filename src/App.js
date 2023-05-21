07b95c3eaf05d924d6e46e4680b5dea0a16577c1
Nathalia Lafosse <lafossenathalia@gmail.com>
2023-05-21 02:07:08 -0400

    created basic calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const Calculator = () => {
+  const [expression, setExpression] = useState("");
+
+  const calculate = () => {
+    try {
+      setExpression(eval(expression).toString());
+    } catch (error) {
+      setExpression("Error");
+    }
+  };
+  const handleButton = (buttonValue) => {
+    setExpression(expression + buttonValue);
+  };
+
+  const clear = () => {
+    setExpression("");
+  };
+
+  useEffect(() => {
+    const handleKeyboardInput = (event) => {
+      const key = event.key;
+
+      if (key === "Enter") {
+        calculate();
+      } else if (key === "Backspace") {
+        setExpression((prevExpression) => prevExpression.slice(0, -1));
+      } else {
+        setExpression((prevExpression) => prevExpression + key);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyboardInput);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyboardInput);
+    };
+  }, [calculate]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-container">
+      <input type="text" className="input-field" value={expression} />
+
+      <div className="keypad">
+        <button onClick={() => handleButton("1")}>1</button>
+        <button onClick={() => handleButton("2")}>2</button>
+        <button onClick={() => handleButton("3")}>3</button>
+        <button onClick={() => handleButton("+")}>+</button>
+        <button onClick={() => handleButton("4")}>4</button>
+        <button onClick={() => handleButton("5")}>5</button>
+        <button onClick={() => handleButton("6")}>6</button>
+        <button onClick={() => handleButton("-")}>-</button>
+        <button onClick={() => handleButton("7")}>7</button>
+        <button onClick={() => handleButton("8")}>8</button>
+        <button onClick={() => handleButton("9")}>9</button>
+        <button onClick={() => handleButton("*")}>*</button>
+        <button onClick={() => handleButton("0")}>0</button>
+        <button onClick={() => handleButton("/")}>/</button>
+        <button onClick={calculate}>=</button>
+        <button onClick={clear}>Clear</button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Calculator;
